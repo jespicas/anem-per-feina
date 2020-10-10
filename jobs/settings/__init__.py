379c47df481f2aaf8813543dae2011a594777857
Francesc Arpí <farpi@apsl.net>
2020-10-10 17:34:39 +0200

    (#37) [Improvement]: sqlite database removed from repository

--- jobs/settings/__init__.py
+++ jobs/settings/__init__.py
@@ -64,10 +64,7 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
+    'default': env.db(),
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #     'NAME': 'ghuyhfun',
